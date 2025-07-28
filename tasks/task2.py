68fd4803e519b6d3e0f54437482dddd2daba49e5
Aisha <aisha.zhumagul@alumni.nu.edu.kz>
2025-07-28 16:48:16 +0500

    add solutions

--- tasks/task2.py
+++ tasks/task2.py
@@ -1,25 +1,11 @@
-# ДАНО:
-# - Строка `text`
-# - Целое или вещественное число `delay` — количество секунд задержки
-
-# ЧТО НУЖНО СДЕЛАТЬ:
-# - Создать асинхронную функцию `delayed_echo(text, delay)`, которая:
-#     → делает паузу на `delay` секунд
-#     → возвращает строку `text`
-
-# - Создать функцию `echo_all()`, которая:
-#     → запускает несколько вызовов `delayed_echo` параллельно
-#     → собирает и возвращает их результаты в списке
-
-# ПРИМЕР:
-# >>> await echo_all()
-# ['hello', 'world', '!']
-
 import asyncio
 
 
 async def delayed_echo(text, delay):
-   pass
+    await asyncio.sleep(delay)
+    return text
+
 
 async def echo_all():
-    pass
+    tasks = [delayed_echo(item, 1) for item in "hello world !".split()]
+    return await asyncio.gather(*tasks)
